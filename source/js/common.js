4d6f062f0868ccd5aee7b192abac3a4e2be8abbb
rhkanrdl <go@footer.kr>
2024-05-10 17:47:53 +0900

    레이아웃 생성

---
+++ source/js/common.js
@@ -0,0 +1,90 @@
+
+$(function(){
+
+    menu();
+    outlink();
+    mobileSubMenu();
+
+
+    $(window).on("resize", function(){
+        $("nav#main-menu h3 a").unbind();
+        menu();
+    });
+
+
+});
+
+
+/* ----- Window Size ----- */
+var windowh = $(window).height();
+var windowW = $(window).width();
+
+$(window).on("resize", function(){
+    windowh = $(window).height();
+    windowW = $(window).width();
+});
+
+
+/* ----- menu ----- */
+function menu() {
+
+
+    $("button.main-menu").on("click", function () {
+        $("#main-menu").toggleClass("on");
+        
+    });
+
+    $("nav#main-menu button.close").on("click", function () {
+
+        $("#main-menu").toggleClass("on");
+    });
+
+    // $("nav#main-menu h3 a").on("click", function (e) {
+        
+    //     $("nav#main-menu h3").removeClass("on");
+
+
+
+    //     if (windowW < 1200 && $(this).parent().parent().find("ul").length) {
+
+    //         $(this).parent().addClass("on");
+    //         e.preventDefault();
+
+    //     }
+    //     else {
+    //         $("nav#main-menu h3 a").unbind();
+
+    //     }
+    // });
+
+    $("nav#main-menu div.menus h3 a").on("mouseenter", function(){
+        $("nav#main-menu div.menus>ul>li").removeClass("on");
+        $(this).parent().parent().addClass("on");
+    });
+
+    $("nav#main-menu div.menus>ul>li ul").on("mouseleave", function(){
+        $(this).parent().removeClass("on");
+    });
+
+}
+
+
+/* ----- outlink ----- */ 
+function outlink() {
+    $(".site-links button").click(function() {
+        $(this).toggleClass("on");
+    });
+}
+
+function mobileSubMenu() {
+
+    $("aside a.on").on("click", function(e) {
+
+        if (windowW < 1200) {
+
+            $("aside ul").toggleClass("on");
+            e.preventDefault();
+
+        }
+    });
+}
